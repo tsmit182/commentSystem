01d30db6f9405c02eab52c1fc4c4bb4ecb1bc73f
Taylor Smith <tsmit182@gmail.com>
2014-08-08 22:51:51 -0400

    content of textarea displays after click on 'submit'

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,32 @@
-var commentTemplate = _.template($("#comment_template").html());
+// var commentTemplate = _.template($("#comment_template").html());
 
 
-var commentValue = document.getElementById("comment_input").value;
+// var commentValue = document.getElementById("comment_input").value;
+// commentValue
 
-var compilingComment = _.template
+//Thought above wasn't functioning quite as I'd thought, so I wrote:
+// var commentValue = comment_input.value
+//But both seem to be functioning similarly
+
+
+// var compilingComment = _.template
 
 
 
-//The following prepends `commentValue` to `.commentsRendered`
-$(".commentsRendered").prepend(commentValue);
+//The following prepends `commentValue` to `.commentsRendered`   [[OR RATHER IT WAS AT ONE POINT]]
+//It seems to prepend when *assigned* as a variable, and wasn't when *called* as a variable
+
+// var comments_rendered = $(".commentsRendered").prepend(commentValue);
+
+
+//Working toward clicking ".comment_submit" in order to prepend the user-entered data
+// $(".comment_submit").click(function(){
+// 	$(".commentsRendered").prepend(commentValue);
+// });
+
+
+//Zomg this seems to work
+$(".comment_submit").click(function(){
+	var commentValue = document.getElementById("comment_input").value;
+	$(".commentsRendered").prepend(commentValue);
+});
--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -58,8 +58,35 @@ var ThisRouter = Backbone.Router.extend({
 	}
 
 });
-var commentTemplate = _.template($("#comment_template").html());
+// var commentTemplate = _.template($("#comment_template").html());
 
 
-var commentValue = document.getElementById("comment_input").value;
+// var commentValue = document.getElementById("comment_input").value;
+// commentValue
 
+//Thought above wasn't functioning quite as I'd thought, so I wrote:
+// var commentValue = comment_input.value
+//But both seem to be functioning similarly
+
+
+// var compilingComment = _.template
+
+
+
+//The following prepends `commentValue` to `.commentsRendered`   [[OR RATHER IT WAS AT ONE POINT]]
+//It seems to prepend when *assigned* as a variable, and wasn't when *called* as a variable
+
+// var comments_rendered = $(".commentsRendered").prepend(commentValue);
+
+
+//Working toward clicking ".comment_submit" in order to prepend the user-entered data
+// $(".comment_submit").click(function(){
+// 	$(".commentsRendered").prepend(commentValue);
+// });
+
+
+//Zomg this seems to work
+$(".comment_submit").click(function(){
+	var commentValue = document.getElementById("comment_input").value;
+	$(".commentsRendered").prepend(commentValue);
+});
